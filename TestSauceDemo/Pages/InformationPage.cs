87f013a972f56b3e3780e1a9368af223543cf17f
NestorkoKhanas <52535323+NestorkoKhanas@users.noreply.github.com>
2025-11-26 16:47:12 +0200

    refactor: move page objects

--- TestSauceDemo/Pages/InformationPage.cs
+++ TestSauceDemo/Pages/InformationPage.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace TestSauceDemo.Pages
 {
-    class InformationPage : BasePage
+    public class InformationPage : BasePage
     {
         public InformationPage(IWebDriver driver) : base(driver) { }
 
@@ -16,14 +16,12 @@ namespace TestSauceDemo.Pages
         private IWebElement lastName => Driver.FindElement(By.Id("last-name"));
         private IWebElement postalCode => Driver.FindElement(By.Id("postal-code"));
         private IWebElement continueInformButton => Driver.FindElement(By.Id("continue"));
-        private IWebElement shopButton => Driver.FindElement(By.XPath("//a[@class='shopping_cart_link']"));
         private IWebElement checkOutButton => Driver.FindElement(By.Id("checkout"));
         private IWebElement errorMessage => Driver.FindElement(By.XPath("//h3[@data-test='error']"));
 
 
         public void ToOrderCards(string first, string last, string postal)
         {
-            shopButton.Click();
             checkOutButton.Click();
             firstName.SendKeys(first);
             lastName.SendKeys(last);
--- TestSauceDemo/Pages/InventoryPage.cs
+++ TestSauceDemo/Pages/InventoryPage.cs
@@ -4,12 +4,15 @@ namespace TestSauceDemo.Pages
 {
     public class InventoryPage : BasePage
     {
+        private IWebElement cartButton => Driver.FindElement(By.XPath("//a[@class='shopping_cart_link']"));
+
         public InventoryPage(IWebDriver driver) : base(driver) { }
         public void AddToCartByName(string productName)
         {
             IWebElement button = Driver.FindElement(By.XPath($"//div[text()='{productName}']/following::button[text()='Add to cart']"));
             button.Click();
         }
+        public void GoToCart() => cartButton.Click();
 
     }
 }
